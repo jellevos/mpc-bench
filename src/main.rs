3c0d3506257871b814a2f6bd708c3369fbe8e661
Jelle Vos <github@jelle-vos.nl>
2021-08-03 15:38:01 +0200

    Add Protocol abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 use std::sync::mpsc::{channel, Sender, Receiver};
 use std::thread::{spawn, JoinHandle};
 use std::collections::HashMap;
+use std::fmt::Debug;
 
 struct Message {
     from_id: usize,
     contents: Vec<u8>,
-}
-
-impl Message {
-    /// Size in bytes
-    fn size(&self) -> usize {
-        self.contents.len()
-    }
 }
 
 struct Mailbox {
@@ -47,56 +41,99 @@ impl Mailbox {
 struct Contacts {
     id: usize,
     senders: Vec<Sender<Message>>,
+    sent_bytes: Vec<usize>,
 }
 
 impl Contacts {
     fn new(id: usize, senders: Vec<Sender<Message>>) -> Self {
+        let sender_count = senders.len();
+
         Contacts {
             id,
-            senders
+            senders,
+            sent_bytes: vec![0; sender_count],
         }
     }
 
-    fn send(&self, message: Vec<u8>, to_party: &usize) {
+    fn send(&mut self, message: &Vec<u8>, to_party: &usize) {
+        let byte_count = message.len();
+
         self.senders[*to_party].send(Message {
             from_id: self.id,
-            contents: message,
+            contents: message.to_vec(),
         }).unwrap();
+
+        self.sent_bytes[*to_party] += byte_count;
+    }
+
+    fn broadcast(&mut self, message: &Vec<u8>) {
+        let byte_count = message.len();
+
+        for sender in &self.senders {
+            sender.send(Message {
+                from_id: self.id,
+                contents: message.to_vec(),
+            }).unwrap();
+        }
+
+        for i in 0..self.sent_bytes.len() {
+            self.sent_bytes[i] += byte_count;
+        }
+    }
+
+    fn get_statistics(self) -> Vec<usize> {
+        self.sent_bytes
     }
 }
 
-fn run(id: usize, n_parties: usize, mailbox: &mut Mailbox, contacts: Contacts) {
-    println!("Hi! I am {}/{}", id, n_parties - 1);
-    for i in (id+1)..n_parties {
-        contacts.send(vec![id as u8], &i);
+trait Protocol<O: 'static + Debug + std::marker::Send> {
+
+    fn evaluate(n_parties: usize) {
+        let mut receivers = vec![];
+        let mut senders: Vec<Vec<Sender<_>>> = (0..n_parties).map(|_| vec![]).collect();
+
+        for _ in 0..n_parties {
+            let (sender, receiver) = channel();
+
+            receivers.push(receiver);
+
+            for j in 0..n_parties {
+                senders[j].push(sender.clone());
+            }
+        }
+
+        let handles: Vec<JoinHandle<_>> = (0..n_parties).zip(receivers.drain(0..n_parties)).zip(senders.drain(0..n_parties))
+            .map(|((i, r), ss)| spawn(move ||
+                Self::run_party(i, n_parties, Mailbox::new(r), Contacts::new(i, ss))))
+            .collect();
+
+        let outputs: Vec<(Vec<usize>, O)> = handles.into_iter().map(|h| h.join().unwrap()).collect();
+
+        println!("{:?}", outputs);
     }
-    for j in 0..id {
-        println!("I am {}/{} and I received a message from {}", id, n_parties - 1, mailbox.receive(&j)[0]);
+
+    fn run_party(id: usize, n_parties: usize, mailbox: Mailbox, contacts: Contacts) -> (Vec<usize>, O);
+
+}
+
+struct Example;
+
+impl Protocol<usize> for Example {
+    fn run_party(id: usize, n_parties: usize, mut mailbox: Mailbox, mut contacts: Contacts) -> (Vec<usize>, usize) {
+        println!("Hi! I am {}/{}", id, n_parties - 1);
+
+        for i in (id + 1)..n_parties {
+            contacts.send(&vec![id as u8], &i);
+        }
+
+        for j in 0..id {
+            println!("I am {}/{} and I received a message from {}", id, n_parties - 1, mailbox.receive(&j)[0]);
+        }
+
+        (contacts.get_statistics(), id + 10)
     }
 }
 
 fn main() {
-    let n_parties = 5;
-
-    let mut receivers = vec![];
-    let mut senders: Vec<Vec<Sender<_>>> = (0..n_parties).map(|_| vec![]).collect();
-
-    for _ in 0..n_parties {
-        let (sender, receiver) = channel();
-
-        receivers.push(receiver);
-
-        for j in 0..n_parties {
-            senders[j].push(sender.clone());
-        }
-    }
-
-    let handles: Vec<JoinHandle<_>> = (0..n_parties).zip(receivers.drain(0..n_parties)).zip(senders.drain(0..n_parties))
-        .map(|((i, r), ss)| spawn(move ||
-            run(i, n_parties, &mut Mailbox::new(r), Contacts::new(i, ss))))
-        .collect();
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
+    Example::evaluate(5);
 }
